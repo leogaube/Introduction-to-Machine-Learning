c5f318863db123537adea72f765fe167e876b6f6
Leo Gaube <gaubeleo@gmail.com>
2022-11-07 12:09:35 +0100

    PM04 done

--- PM04/mlis/kernels/kernel_logistic_regression.py
+++ PM04/mlis/kernels/kernel_logistic_regression.py
@@ -13,9 +13,10 @@ def kernel_lr(X, y, sigma, lam):
     @param lam: regularization parameter
     @return: A model h: X -> prediction
     """
-    α = np.zeros(X.shape[0], )
-    α = minimize(J, α, args=(X, y, sigma, lam), jac=dJ, method='bfgs').x
-    return None  # <<<--- Replace this by your own result.
+    α = np.zeros(X.shape[0])
+    α = minimize(J, α, args=(X, y, sigma, lam), jac=dJ, method="bfgs").x
+    return lambda Z: α @ sq_exp(X, Z, sigma)
+    # <<<--- Replace this by your own result.
 
 
 def J(α, X, y, sigma, lam):
@@ -28,19 +29,20 @@ def J(α, X, y, sigma, lam):
     @param lam: regularization parameter
     @return: J(w)
     """
+    K = sq_exp(X, X, sigma)
 
-    return None  # <<<--- Replace this by your own result.
+    objective = np.mean(np.log(1 + np.exp(-y * (α @ K))))
+    regularization = lam * ((α.T @ K) @ α)
+
+    return objective + regularization
+    # <<<--- Replace this by your own result.
 
 
 def dJ(α, X, y, sigma, lam):
-    """
-    Gradient of the kernel logistic objective function
-    @param α: kernel weights / parameters
-    @param X: data matrix with x_i^T in the ith row
-    @param y: vector of labels
-    @param sigma: kernel bandwidth / length-scale
-    @param lam: regularization parameter
-    @return:  ∇J(w)
-    """
+    m, n = X.shape
+    K = sq_exp(X, X, sigma)
 
-    return None  # <<<--- Replace this by your own result.
+    gradient = (-y @ K) / (1 + np.exp(y @ (α @ K)))
+    regularization = 2 * lam * np.dot(K, α)
+
+    return gradient / m + regularization
