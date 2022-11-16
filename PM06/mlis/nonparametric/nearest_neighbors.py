8785ec1b26c58f0ddffa1d5c5e6c5967f5480624
Leo Gaube <gaubeleo@gmail.com>
2022-11-16 12:32:34 +0100

    PM06 done

--- PM06/mlis/nonparametric/nearest_neighbors.py
+++ PM06/mlis/nonparametric/nearest_neighbors.py
@@ -8,7 +8,7 @@ def knn_classify(X, y, Z, k, dist):
     k nearest neighbor classification
     @param X: m by n matrix of inputs
     @param y: m by 1 matrix of labels
-    @param Z: mz by n matrix of inputs
+    @param Z: m by n matrix of inputs
     @param k: how many neighbors are considered?
     @param dist: A distance function from scipy.spatial.distance.cdist
     @return: the label prediction by k nearest neighbor classification
@@ -21,6 +21,7 @@ def knn_classify(X, y, Z, k, dist):
         idx = np.argpartition(distances, k, axis=1)[:, :k]
     else:
         idx = np.indices((m, m))[1]  # <-- column indices
+
     return np.mean(y[idx], axis=1)
 
 
